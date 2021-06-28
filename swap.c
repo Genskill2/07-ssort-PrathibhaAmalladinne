cf8d52cb6e2d1b3763c87405677cb671f6d17130
PrathibhaAmalladinne <84717502+PrathibhaAmalladinne@users.noreply.github.com>
2021-06-28 17:44:10 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,22 @@
+int swap_max(int arr[], int l, int n )
+{
+  int temp=arr[n];
+  int index,i;
+    for(i=n+1;i<l;i++)
+    {
+	    if(arr[i]>temp)
+	    {
+		    temp=arr[i];
+		    index=i;
+	    }
+   }
+  arr[index]=arr[n];
+  arr[n]=temp;
+}
+int ssort(int arr[],int l)
+{
+  for(int position=0;position<l-1;position++)
+  {	
+	  swap_max(arr,l,position);
+  }
+}
